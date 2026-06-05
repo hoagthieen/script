10c8d6d66880440661156bf93d6417b9cddc6e56
hoagthieen <hoagthieenyeuem@gmail.com>
2026-06-05 13:55:36 +0700

    Update Veltrix-bloxfruits.lua

--- Veltrix-bloxfruits.lua
+++ Veltrix-bloxfruits.lua
@@ -39,5 +39,4 @@ fadeTween.Completed:Wait()
 
 gui:Destroy()
 
-
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/5c416c28167ec6ee9051edf5c83bd362bf5a7c4d1b3e63fe37fd4a1620a06ede/download"))()
