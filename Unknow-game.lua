79c8cd7cfb37daa072403a5041a02467d614068a
hoagthieen <hoagthieenyeuem@gmail.com>
2026-06-07 19:29:57 +0700

    Update Unknow-game.lua

--- Unknow-game.lua
+++ Unknow-game.lua
@@ -1,1 +1,24 @@
-print("NotSupport this game")
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "Veltrix X Hub | NoSupportThisGame",
+    SubTitle = "by hoagthieen",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Aqua",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+
+Fluent:Notify({
+        Title = "Notification",
+        Content = "No Support This Game",
+        SubContent = "Please participate in the experience supported by this script", -- Optional
+        Duration = 10 -- Set to nil to make the notification not disappear
+})
+
+-- Fluent provides Lucide Icons, they are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "eye" }),
+}
+
