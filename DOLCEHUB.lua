ab13e32ce17e1e5c18948e1ba86c7d694c016262
hoagthieen <hoagthieenyeuem@gmail.com>
2026-07-27 19:04:12 +0700

    Update script URLs for Dolce Hub

--- DOLCEHUB.lua
+++ DOLCEHUB.lua
@@ -4,24 +4,23 @@ local PlaceId = game.PlaceId
 local HttpService = game:GetService("HttpService")
 
 local GameScripts = {
-    [2753915549] = "https://raw.githubusercontent.com/thieengw9990/Astra-Hub/refs/heads/main/GameHub/BloxFruits.lua",
-    [4442272183] = "https://raw.githubusercontent.com/thieengw9990/Astra-Hub/refs/heads/main/GameHub/BloxFruits.lua",
-    [7449423635] = "https://raw.githubusercontent.com/thieengw9990/Astra-Hub/refs/heads/main/GameHub/BloxFruits.lua",
-    [131623223084840] = "https://raw.githubusercontent.com/thieengw9990/Astra-Hub/refs/heads/main/GameHub/Escape-Tsunami-For-Brainrots.lua",
+    [2753915549] = "https://raw.githubusercontent.com/hoagthieen/script/refs/heads/main/Dolce-Hub/Bloxfruits.lua",
+    [4442272183] = "https://raw.githubusercontent.com/hoagthieen/script/refs/heads/main/Dolce-Hub/Bloxfruits.lua",
+    [7449423635] = "https://raw.githubusercontent.com/hoagthieen/script/refs/heads/main/Dolce-Hub/Bloxfruits.lua",
     ["Universal"] = "https://raw.githubusercontent.com/thieengw9990/Astra-Hub/refs/heads/main/GameHub/unknown-game"
 }
 
 local scriptUrl = GameScripts[PlaceId] or GameScripts["Universal"]
 
-print("[ASTRA X HUB] Router active. Detected PlaceId: " .. tostring(PlaceId))
-print("[ASTRA X HUB] Routing to branch...")
+print("[Dolce Hub] Router active. Detected PlaceId: " .. tostring(PlaceId))
+print("[Dolce hub] Routing to branch...")
 
 local success, err = pcall(function()
     loadstring(game:HttpGet(scriptUrl))()
 end)
 
 if not success then
-    warn("[ASTRA X HUB] Failed to load game branch: " .. tostring(err))
+    warn("[Dolce Hub] Failed to load game branch: " .. tostring(err))
     
     if scriptUrl ~= GameScripts["Universal"] then
         loadstring(game:HttpGet(GameScripts["Universal"]))()
